2046e93b58c55bfba2fd96f2ac99e411b5e81931
anandb7248 <anandb7248@gmail.com>
2020-05-07 13:46:14 -0700

    Modified animations for header buttons and set event transition based on scroll

--- personal-site/src/components/header.js
+++ personal-site/src/components/header.js
@@ -1,20 +1,66 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Link from 'gatsby-link'
 import icon from '../images/Icon.png'
 import './header.css'
+import { renderStatic } from 'react-helmet'
 
-const Header = () => (
-  <div className="Header">
-    <div className="HeaderGroup">
-      <Link to="/"><img src={icon} height="65x"/></Link>
-      <Link to="/about">About</Link>
-      <Link to="/skills">Skills</Link>
-      <Link to="/experience">Experience</Link>
-      <Link to="/projects">Projects</Link>
-      <Link to="/contact">Contact</Link>
-      <Link to="/resume"><button>Resume</button></Link>
-    </div>
-  </div>
-)
+// const Header = () => (
+//   <div className="Header">
+//     <div className="HeaderGroup">
+//       <Link to="/"><img src={icon} height="65x"/></Link>
+//       <Link className="text" to="/about">About</Link>
+//       <Link className="text" to="/skills">Skills</Link>
+//       <Link className="text" to="/experience">Experience</Link>
+//       <Link className="text" to="/projects">Projects</Link>
+//       <Link className="text" to="/contact">Contact</Link>
+//       <Link to="/resume"><button>Resume</button></Link>
+//     </div>
+//   </div>
+// )
+
+class Header extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      hasScrolled: false
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll = (event) => {
+    const scrollTop = window.pageYOffset;
+
+    if (scrollTop > 50){
+      this.setState({
+        hasScrolled: true
+      })
+    }else{
+      this.setState({
+        hasScrolled: false
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div className={this.state.hasScrolled ? 'Header HeaderScrolled' : 'Header'}>
+          <div className='HeaderGroup'>
+            <Link to="/"><img src={icon} height="65x"/></Link>
+            <Link className="text" to="/about">About</Link>
+            <Link className="text" to="/skills">Skills</Link>
+            <Link className="text" to="/experience">Experience</Link>
+            <Link className="text" to="/projects">Projects</Link>
+            <Link className="text" to="/contact">Contact</Link>
+            <Link to="/resume"><button>Resume</button></Link>
+          </div>
+        </div>
+    );
+  } 
+    
+}
 
 export default Header
