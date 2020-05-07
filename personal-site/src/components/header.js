17a456c00bb52e2f63ce28531c5dc73e15b7429c
anandb7248 <anandb7248@gmail.com>
2020-05-07 13:52:42 -0700

    Implement resume view feature

--- personal-site/src/components/header.js
+++ personal-site/src/components/header.js
@@ -3,20 +3,7 @@ import Link from 'gatsby-link'
 import icon from '../images/Icon.png'
 import './header.css'
 import { renderStatic } from 'react-helmet'
-
-// const Header = () => (
-//   <div className="Header">
-//     <div className="HeaderGroup">
-//       <Link to="/"><img src={icon} height="65x"/></Link>
-//       <Link className="text" to="/about">About</Link>
-//       <Link className="text" to="/skills">Skills</Link>
-//       <Link className="text" to="/experience">Experience</Link>
-//       <Link className="text" to="/projects">Projects</Link>
-//       <Link className="text" to="/contact">Contact</Link>
-//       <Link to="/resume"><button>Resume</button></Link>
-//     </div>
-//   </div>
-// )
+import resumeFile from '../resume/Anand_Batjargal_Resume.pdf'
 
 class Header extends React.Component {
   constructor(props){
@@ -50,12 +37,12 @@ class Header extends React.Component {
       <div className={this.state.hasScrolled ? 'Header HeaderScrolled' : 'Header'}>
           <div className='HeaderGroup'>
             <Link to="/"><img src={icon} height="65x"/></Link>
-            <Link className="text" to="/about">About</Link>
-            <Link className="text" to="/skills">Skills</Link>
-            <Link className="text" to="/experience">Experience</Link>
-            <Link className="text" to="/projects">Projects</Link>
-            <Link className="text" to="/contact">Contact</Link>
-            <Link to="/resume"><button>Resume</button></Link>
+            <Link className="text" to="/">About</Link>
+            <Link className="text" to="/">Skills</Link>
+            <Link className="text" to="/">Experience</Link>
+            <Link className="text" to="/">Projects</Link>
+            <Link className="text" to="/">Contact</Link>
+            <a href={resumeFile} target="_blank" rel="noopener noreferrer"><button>Resume</button></a>
           </div>
         </div>
     );
