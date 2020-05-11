b6f775d22e1f3c3247192f81172b4e8042741c0a
anandb7248 <anandb7248@gmail.com>
2020-05-10 18:10:57 -0700

    Fix for hero group

--- personal-site/src/components/hero.js
+++ personal-site/src/components/hero.js
@@ -13,20 +13,20 @@ const Hero = () => {
                 <h3>Hi, my name is</h3>
                 <h1>Anand Batjargal</h1>
             </div>
-        <div className="bottom">
-            <h3>I am a software engineer based in San Francisco Bay Area.</h3>
-            <h4>I enjoy building iOS and Full-Stack Web applications with an emphasis on great design.</h4>
+            <div className="bottom">
+                <h3>I am a software engineer based in San Francisco Bay Area.</h3>
+                <h4>I enjoy building iOS and Full-Stack Web applications with an emphasis on great design.</h4>
+            </div>
+            <div className="logos">
+                <a href="https://github.com/anandb7248" target="_blank" rel="noopener noreferrer"><img src={github} height="50px"/></a>
+                <a href = "mailto:anandb7248@gmail.com"><img src={email} height="50px"/></a>
+                <a href="https://www.linkedin.com/in/anandbatjargal/" target="_blank" rel="noopener noreferrer"><img src={linkedin} height="50px"/></a>
+            </div>
+            <div className="down">
+                <a href=""><img src={down} height="45px"/></a>
+            </div>
         </div>
-        <div className="logos">
-            <a href="https://github.com/anandb7248" target="_blank" rel="noopener noreferrer"><img src={github} height="50px"/></a>
-            <a href = "mailto:anandb7248@gmail.com"><img src={email} height="50px"/></a>
-            <a href="https://www.linkedin.com/in/anandbatjargal/" target="_blank" rel="noopener noreferrer"><img src={linkedin} height="50px"/></a>
-        </div>
-        <div className="down">
-            <a href=""><img src={down} height="45px"/></a>
-        </div>
-    </div>
-  </div>)
+    </div>)
 }
 
 export default Hero
