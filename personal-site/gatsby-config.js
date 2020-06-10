969ada99c113fd33a57ecf244974f9b59f21530a
anandb7248 <anandb7248@gmail.com>
2020-06-09 19:09:41 -0700

    upgrade gatsby to latest

--- personal-site/gatsby-config.js
+++ personal-site/gatsby-config.js
@@ -1,8 +1,22 @@
+const path = require(`path`)
+
 module.exports = {
   siteMetadata: {
     title: 'Anand Batjargal | SWE',
-    description: 'Personal website for Anand Batjargal. To showcase abilities, experience, projects, and contact information. More coming on the way.',
-    keywords: 'anand batjargal, anand, batjargal, software, software engineer, software developer, iOS engineer, iOS developer, web developer, full stack, front end, back end, full stack web developer, mongolian software engineer'
+    description:
+      'Personal website for Anand Batjargal. To showcase abilities, experience, projects, and contact information. More coming on the way.',
+    keywords:
+      'anand batjargal, anand, batjargal, software, software engineer, software developer, iOS engineer, iOS developer, web developer, full stack, front end, back end, full stack web developer, mongolian software engineer',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: path.join(__dirname, `src`, `images`),
+      },
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+  ],
 }
--- personal-site/src/components/header.js
+++ personal-site/src/components/header.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react'
-import Link from 'gatsby-link'
 import icon from '../images/Icon.png'
 import './header.css'
-import { renderStatic } from 'react-helmet'
 import resumeFile from '../resume/Anand_Batjargal_Resume.pdf'
 
 class Header extends React.Component {
@@ -44,7 +42,6 @@ class Header extends React.Component {
           <a href="/#about">About</a>
           <a href="/#skills">Skills</a>
           <a href="/#experience">Experience</a>
-          {/* <Link className="text" to="/">Projects</Link> */}
           <a href="/#contact">Contact</a>
           <a href={resumeFile} target="_blank" rel="noopener noreferrer">
             <button>Resume</button>
